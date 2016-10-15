d77cff59d750ebfd32df0832268ca78379fb2600
coocla <327052034@qq.com>
2016-10-15 21:53:15 +0800

    调整本地IDE缩进配置

--- cloudtea/app.py
+++ cloudtea/app.py
@@ -10,11 +10,10 @@ from cloudtea.views import login, main
 QTextCodec.setCodecForTr(QTextCodec.codecForName("utf8"))
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	
-	login_view = login.LoginWindow()
-	login_view.setStyleSheet(qdarkstyle.load_stylesheet())
-	if login_view.exec_() == QDialog.Accepted:
-		widget = main.MainWindow()
-		widget.show()
-		app.exec_()
+    app = QApplication(sys.argv)
+    login_view = login.LoginWindow()
+    login_view.setStyleSheet(qdarkstyle.load_stylesheet())
+    if login_view.exec_() == QDialog.Accepted:
+        widget = main.MainWindow()
+        widget.show()
+        app.exec_()
--- cloudtea/test.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print("1111111111111")
-import time
-time.sleep(30)
